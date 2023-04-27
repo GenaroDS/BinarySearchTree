9b3d696a6f9896fee20e03e23551882d70652725
Genaro Di Stilio <genarodistilio@gmail.com>
2023-04-27 10:26:21 -0300

    Full structure uploaded

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        System.out.println("We add the numbers: 4, 2, 6, 0, 8.");
+        tree.insert(4);
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(0);
+        tree.insert(8);
+        tree.printTree();
+        System.out.println("");
+        System.out.println("And then we add the values 7, 9, 1, 3.");
+        tree.insert(7);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(3);
+        tree.printTree();
+        System.out.println("");
+        System.out.println("The values are succesfully added to the tree.");
+    }
+}
---
+++ src/Tree.java
@@ -0,0 +1,71 @@
+public class Tree {
+    Node root;
+
+    Tree() {
+        this.root = null;
+    }
+
+    void insert(int key) {
+        Node newNode = new Node(key);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+        Node currentNode = root;
+        Node parentNode = null;
+        while (currentNode != null) {
+            parentNode = currentNode;
+            if (key < currentNode.key)
+                currentNode = currentNode.left;
+            else if (key > currentNode.key)
+                currentNode = currentNode.right;
+            else
+                return;
+        }
+        if (key < parentNode.key)
+            parentNode.left = newNode;
+        else
+            parentNode.right = newNode;
+    }
+
+    boolean search(int key) {
+        Node currNode = root;
+        while (currNode != null) {
+            if (key == currNode.key)
+                return true;
+            else if (key < currNode.key)
+                currNode = currNode.left;
+            else
+                currNode = currNode.right;
+        }
+        return false;
+    }
+
+    public void printTree(Node root, int space) {
+        int count = 10;
+        if (root == null)
+            return;
+
+        // Increase distance between levels  
+        space += count;
+
+        // Process right child first 
+        printTree(root.right, space);
+
+        // Print current node after space  
+        // count 
+        System.out.println();
+        for (int i = count; i < space; i++) 
+            System.out.print(" ");
+        System.out.println(root.key);
+
+        // Process left child 
+        printTree(root.left, space);
+    }
+
+    public void printTree() {
+        printTree(root, 0);
+    }
+
+
+}
